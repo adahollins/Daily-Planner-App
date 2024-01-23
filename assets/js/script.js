fa85f94c990b647be8c09d08d5b64b99bfa3b16d
adahollins <adrianna.hollins@hotmail.com>
2024-01-23 15:51:50 +0000

    Improved container

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,41 @@ var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 for (let i = 0; i < times.length; i++) {
 
-    var div = $("<div>");
     var textHours = $("<h4>");
     var saveBtn = $("<button>");
-    var input = $("<input>");
+    var input = $("<textarea>");
 
-    div.addClass("description timeblock");
 
-    textHours.addClass("hour");
-    textHours.text(times[i]);
+textHours.addClass("hour");
+textHours.text(times[i]);
 
-    input.addClass("row")
+input.addClass("description");
 
-    saveBtn.addClass("saveBtn");
-    
-    $(".container").append(div);
-    $(".container").append(textHours);
-    $(".container").append(input);
-    $(".container").append(saveBtn);
+saveBtn.addClass("saveBtn");
+
+$(".container").append(textHours);
+   // $(".container").append(input);
+   // $(".container").append(saveBtn);
+
+    var newsDiv = $('<div class="row time-block">').appendTo($(".container"));
+    $(newsDiv).each(function() {
+        newsDiv.wrap("<div>");
+        newsDiv.append(input);
+        newsDiv.append(saveBtn); 
+        newsDiv.wrap("</div>");
+    });
 
 //Changes colour depending on time
-    var currentTime = dayjs().format('hA');
-    if (textHours == currentTime) {
-        input.addClass("present");
-    } else if (textHours >= currentTime) {
-        input.addClass("past");
-    } else {
-        input.addClass("future");
-    };
+};
+for (let i = 0; i < textHours.length; i++) {
+var currentTime = dayjs().format('hA');
+if (textHours == currentTime) {
+    input.addClass("present");
+} else if (textHours >= currentTime) {
+    input.addClass("past");
+} else {
+    input.addClass("future");
+};
 };
 
 });
